18bcd757742fb5a53034f897cbc8558d14e8923d
cassius23 <emilekm@gmail.com>
2026-01-05 13:32:37 +0100

    Comment out render command

--- cmd/render.go
+++ cmd/render.go
@@ -1,73 +1,73 @@
 package cmd
 
-import (
-	"fmt"
-
-	"github.com/spf13/cobra"
-)
-
-type renderOpts struct {
-	*serverOpts
-	dryRun         bool
-	reloadableOnly bool
-}
-
-func newRenderOpts() *renderOpts {
-	return &renderOpts{
-		serverOpts: newServerOpts(),
-	}
-}
-
-func init() {
-	rootCmd.AddCommand(renderCmd())
-}
-
-func renderCmd() *cobra.Command {
-	opts := newRenderOpts()
-
-	cmd := &cobra.Command{
-		Use:          "render",
-		Short:        "Render templates",
-		SilenceUsage: true,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return opts.Run(cmd)
-		},
-	}
-
-	opts.AddFlags(cmd)
-
-	return cmd
-}
-
-func (opts *renderOpts) AddFlags(cmd *cobra.Command) {
-	opts.serverOpts.AddFlags(cmd)
-
-	cmd.Flags().BoolVar(&opts.dryRun, "dry-run", false, "Print out rendered files")
-	cmd.Flags().BoolVar(&opts.reloadableOnly, "reloadable-only", false, "Only render reloadable templates")
-}
-
-func (opts *renderOpts) Run(cmd *cobra.Command) error {
-	sv, err := opts.Server()
-	if err != nil {
-		return err
-	}
-
-	if opts.dryRun {
-		outputs, err := sv.DryRender()
-		if err != nil {
-			return err
-		}
-		for _, out := range outputs {
-			fmt.Printf("File: %s\n---\n%s", out.Destination, string(out.Content))
-		}
-	} else {
-		err := sv.Render(opts.reloadableOnly)
-		if err != nil {
-			return err
-		}
-	}
-
-	cmd.Println("Rendered templates.")
-
-	return nil
-}
+// import (
+// 	"fmt"
+//
+// 	"github.com/spf13/cobra"
+// )
+//
+// type renderOpts struct {
+// 	*serverOpts
+// 	dryRun         bool
+// 	reloadableOnly bool
+// }
+//
+// func newRenderOpts() *renderOpts {
+// 	return &renderOpts{
+// 		serverOpts: newServerOpts(),
+// 	}
+// }
+//
+// func init() {
+// 	rootCmd.AddCommand(renderCmd())
+// }
+//
+// func renderCmd() *cobra.Command {
+// 	opts := newRenderOpts()
+//
+// 	cmd := &cobra.Command{
+// 		Use:          "render",
+// 		Short:        "Render templates",
+// 		SilenceUsage: true,
+// 		RunE: func(cmd *cobra.Command, args []string) error {
+// 			return opts.Run(cmd)
+// 		},
+// 	}
+//
+// 	opts.AddFlags(cmd)
+//
+// 	return cmd
+// }
+//
+// func (opts *renderOpts) AddFlags(cmd *cobra.Command) {
+// 	opts.serverOpts.AddFlags(cmd)
+//
+// 	cmd.Flags().BoolVar(&opts.dryRun, "dry-run", false, "Print out rendered files")
+// 	cmd.Flags().BoolVar(&opts.reloadableOnly, "reloadable-only", false, "Only render reloadable templates")
+// }
+//
+// func (opts *renderOpts) Run(cmd *cobra.Command) error {
+// 	sv, err := opts.Server()
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	if opts.dryRun {
+// 		outputs, err := sv.DryRender()
+// 		if err != nil {
+// 			return err
+// 		}
+// 		for _, out := range outputs {
+// 			fmt.Printf("File: %s\n---\n%s", out.Destination, string(out.Content))
+// 		}
+// 	} else {
+// 		err := sv.Render(opts.reloadableOnly)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+//
+// 	cmd.Println("Rendered templates.")
+//
+// 	return nil
+// }
